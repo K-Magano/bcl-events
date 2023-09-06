7c89307c90eb28c5e229de754566a636f5cdc613
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-09-06 13:47:39 +0200

    Added eventid data
    Co-authored-by: Chad Bazil Bosch <Cbazil@users.noreply.github.com>

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,16 +2,16 @@ import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
 import ErrorAlert from "@/components/ui/error-alert";
-import { getEventById } from "@/dummy-data";
-import { useRouter } from "next/router";
+import { getEventById, getAllEvents } from "../../helpers/api-util";
+
 import { Fragment } from "react";
 
-function EventDetailPage(events) {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent; //from async at the bottom
 
-  if (!eventId) {
+  console.log("e: ", event);
+
+  if (!event) {
     return (
       <ErrorAlert>
         <h3> Event not found</h3>;
@@ -21,10 +21,12 @@ function EventDetailPage(events) {
   return (
     //all components below were created by the lesson.
     <Fragment>
+      <p>We are in pages/events/[eventid]</p>
+
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
-        address={event.location}
+         ={event.location}
         image={event.image}
         imageAlt={event.title}
       />
@@ -33,6 +35,26 @@ function EventDetailPage(events) {
       </EventContent>
     </Fragment>
   );
+}
+//context because we need a specific id
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getAllEvents(eventId);
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+//to help next know what eventid (param values)to look out for
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
 }
 
 export default EventDetailPage;
